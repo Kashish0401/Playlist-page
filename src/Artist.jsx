e40b0df6c1c2b13c04302653313e8bead6d76c22
Kashish <kashish042001@gmail.com>
2022-11-26 16:12:53 +0530

    changes

--- src/Artist.jsx
+++ src/Artist.jsx
@@ -1,14 +1,13 @@
 import "./Artist.css";
 
 const Artist = () => {
-    return(
+    return (
         <div className="Artists">
             <li className="Artists-li" >
                 <a href="#" title="The weekend" className="Item">
                     <div className="Artists-block">
-                        <img className="Artists-image" src="https://img.wynk.in/unsafe/80x80/filters:no_upscale():strip_exif():format(webp)/http://s3.ap-south-1.amazonaws.com/discovery-prod-zion/zion/1667205846568-Imagine_Dragons-wa_9d276132.png" alt="Imagine Dragons"/>
-                        <span className="Artists-title">Imagine Dragons</span>
-                    </div>
+                        <img className="Artists-image" src="https://img.wynk.in/unsafe/80x80/filters:no_upscale():strip_exif():format(webp)/http://s3.ap-south-1.amazonaws.com/discovery-prod-zion/zion/1667205846568-Imagine_Dragons-wa_9d276132.png" alt="Imagine Dragons" />
+                        <span className="Artists-title">Imagine Dragons</span></div>
                 </a>
             </li>
         </div>
--- src/SongRow.jsx
+++ src/SongRow.jsx
@@ -2,7 +2,7 @@ import "./SongRow.css";
 
 const SongRow = () =>{
     return(
-        <div className="SongRow" style={{position: 'relative', top: '1.5rem'}}>
+        <div className="SongRow">
             <div className="SongBlock">
                 <img src="https://i.ytimg.com/vi/fQWhenQgXo8/sddefault.jpg?sqp=-oaymwEWCJADEOEBIAQqCghqEJQEGHgg6AJIWg&rs=AMzJL3mx5qTlRvvP8TQX-8zEPJHAyGPgjQ" alt="Photograph" className="Song-image" />
                 <div className="Song-title">
@@ -10,6 +10,10 @@ const SongRow = () =>{
                         <div className="Song-title"><a title="Photograph" href="#">Photograph</a></div>
                         <div className="Song-artist"><a title="Ed Sheran" href="#"> Ed Sheran - Photograph</a></div>
                     </div>
+                </div>
+                <div className="Side-icons">
+                    <div><button  className="icon-1"><img src="https://img.icons8.com/material-rounded/24/null/downloading-updates.png"/></button></div>
+                    <div><button  className="icon-2"><img src="https://img.icons8.com/ios-glyphs/30/null/menu-2.png"/></button></div>
                 </div>
             </div>
         </div>
